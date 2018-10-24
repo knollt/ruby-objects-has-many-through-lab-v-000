92d34c07115217fccc6ca2bce17e8ea4f44b19e3
Tim Knoll <timothy.knoll@gmail.com>
2018-10-24 15:25:27 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -33,3 +33,17 @@ require 'pry'
 #   end
 #
 # end
+
+class Artist
+
+  attr_accessor :name
+
+  @@all_artists = []
+
+  def initialize (name)
+    @name = name
+    @@all_artists << self
+  end
+
+  
+end
